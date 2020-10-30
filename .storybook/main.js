96a10b971611340d18c120ab3ac9399120a7054e
DragonFyZex <1271758-DragonFyZex@users.noreply.gitlab.com1271758-DragonFyZex@users.noreply.gitlab.com>
2020-10-29 19:50:01 -0700

    Removed Storybook

--- .storybook/main.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    stories: ['../components/**/**/**/*.stories.@(js|mdx)'],
-    addons: ['storybook-addon-styled-component-theme/dist/register', '@storybook/addon-actions/register', '@storybook/addon-links/register', '@storybook/addon-knobs/register', '@storybook/addon-viewport/register', '@storybook/addon-a11y/register']
-};
