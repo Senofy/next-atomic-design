7ce9c0e2a89324ac7a4039fa56704d0ba5157464
DragonFyZex <dragon@kbve.com>
2020-11-03 02:23:47 -0800

    Removed colors and text and moved into theme

--- themes/default.js
+++ themes/default.js
@@ -4,30 +4,61 @@ import { reversePalette } from 'styled-theme/composer'
 const theme = {}
 
 theme.palette = {
-    primary: ['#1976d2', '#2196f3', '#71bcf7', '#c2e2fb'],
-    secondary: ['#c2185b', '#e91e63', '#f06292', '#f8bbd0'],
-    danger: ['#d32f2f', '#f44336', '#f8877f', '#ffcdd2'],
+    primary: [
+        '#E5F1FA',
+        '#BED7FA',
+        '#93B8F9',
+        '#6596FB',
+        '#3772FF',
+        '#215AF0',
+        '#0237E1',
+        '#001FCE',
+        '#0011A0'
+    ],
+    secondary: [
+        '#E9F6EE',
+        '#C2E6CF',
+        '#9AD5AF',
+        '#6FC48E',
+        '#44AF69',
+        '#319D54',
+        '#1C8741',
+        '#0B7A32',
+        '#006224'
+    ],
+    danger: [
+        '#F7E1E6',
+        '#EFB6BF',
+        '#E68894',
+        '#DF5867',
+        '#D62839',
+        '#C51725',
+        '#B2000D',
+        '#9B0003',
+        '#750200'
+    ],
     alert: ['#ffa000', '#ffc107', '#ffd761', '#ffecb3'],
     success: ['#388e3c', '#4caf50', '#7cc47f', '#c8e6c9'],
     white: ['#fff', '#fff', '#eee'],
     grayscale: [
-        '#212121',
-        '#414141',
-        '#616161',
-        '#9e9e9e',
-        '#bdbdbd',
-        '#e0e0e0',
-        '#eeeeee',
-        '#ffffff',
+        '#F3F4F4',
+        '#DADBDD',
+        '#BEC1C3',
+        '#A2A5AA',
+        '#82868C',
+        '#6C7076',
+        '#54575E',
+        '#42464C',
+        '#2E323A'
     ],
 }
 
 theme.reversePalette = reversePalette(theme.palette)
 
 theme.fonts = {
-    primary: 'Helvetica Neue, Helvetica, Roboto, sans-serif',
-    pre: 'Consolas, Liberation Mono, Menlo, Courier, monospace',
-    quote: 'Georgia, serif',
+    primary: 'Inter, sans-serif',
+    pre: 'Poppins, sans-serif',
+    quote: 'Inter, sans-serif',
 }
 
 theme.sizes = {
