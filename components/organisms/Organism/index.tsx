299e520390d4bffe474d821646ae92f0f2eb7ab1
WessCoby <cobygiven@gmail.com>
2020-11-07 20:23:06 +0000

    Renamed to .tsx

---
+++ components/organisms/Organism/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+import { Wrapper } from './styles';
+
+
+const Organism: FC = () => {
+  return (
+    <Wrapper>
+      content
+    </Wrapper>
+  )
+}
+
+export default Organism
---
+++ components/organisms/Organism/styles.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { fontFamily, color } from 'styled-system';
+
+
+export const Wrapper = styled.div`
+  ${fontFamily};
+  ${color};
+`
