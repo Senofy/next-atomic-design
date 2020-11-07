edc6faaa9bc4c7080432471f9c62e3bac406440a
WessCoby <cobygiven@gmail.com>
2020-11-07 20:05:03 +0000

    Added typings for button atom

---
+++ components/atoms/Button/index.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+import {
+  color, fontFamily, fontSize, fontWeight, lineHeight, padding
+} from 'styled-system';
+
+
+export interface ButtonProps {
+  fontWeight?: number;
+  padding?: number;
+}
+
+export const Button = styled.button<ButtonProps>`
+    ${color};
+    ${fontFamily};
+    ${fontSize};
+    ${fontWeight};
+    ${lineHeight};
+    ${padding};
+    border-radius: 0.25rem;
+    background-color: blue
+`;
