b6b1da5783b7f657177abbac719316f7d9d1a25c
WessCoby <cobygiven@gmail.com>
2020-11-07 20:18:06 +0000

    Renamed to .tsx

---
+++ components/organisms/Header/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+import { Wrapper } from './styles';
+import { Button, Heading } from '../../atoms';
+
+
+const Header: FC = () => {
+  return (
+    <Wrapper>
+      <Heading.H4 fontWeight={600}>
+        Welcome to Dashboard
+      </Heading.H4>
+      <Button fontWeight={500} color="white" padding={2}>GENERATE REPORT</Button>
+    </Wrapper>
+  )
+}
+
+export default Header;
