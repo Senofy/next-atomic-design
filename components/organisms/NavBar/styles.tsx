5177122ac3a505e81b603c34a1b6dd3f2dc9f599
WessCoby <cobygiven@gmail.com>
2020-11-07 20:20:41 +0000

    Renamed to .tsx

---
+++ components/organisms/NavBar/styles.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+
+export const Wrapper = styled.div`
+    background-color: white;
+
+    height: 60px;
+    width: 100%;
+    border-bottom: 1px solid rgba(72, 94, 144, 0.16);
+    padding: 0 20px;
+    display: grid;
+    align-items: center;
+
+    @media screen and (min-width: 992px) {
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: 1fr;
+    }
+`
+export const ImageWrapper = styled.div`
+  width: 150px;
+  height: 50px;
+`;
+
+export const LinkWrapper = styled.div`
+    justify-self: center;
+
+    display: flex;
+    & > :not(:first-child) {
+        margin-left: 30px;
+    }
+`
+
+export const ExtraWrapper = styled.div`
+    justify-self: end;
+`
