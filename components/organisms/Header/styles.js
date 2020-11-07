d0b00c80fd65349cecf3651997f5e533c17b4d35
WessCoby <cobygiven@gmail.com>
2020-11-07 19:26:44 +0000

    Renamed jsx files to tsx

--- components/organisms/Header/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { font, palette } from 'styled-theme'
-
-export const Wrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    
-    width: 100%;
-`
--- components/organisms/Organism/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-import { font, palette } from 'styled-theme'
-
-export const Wrapper = styled.div`
-    font-family: ${font('primary')};
-    color: ${palette('grayscale', 0)};
-`
