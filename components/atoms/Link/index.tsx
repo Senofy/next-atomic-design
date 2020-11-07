746bc5ed7f88ef91c01158616ed58316769fe238
WessCoby <cobygiven@gmail.com>
2020-11-07 20:06:58 +0000

    Added typings for Link atom

---
+++ components/atoms/Link/index.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import {
+  color, fontFamily, fontSize, fontWeight, lineHeight
+} from 'styled-system'
+
+
+interface LinkProps {
+  href: string;
+}
+
+export const NavLink = styled.a<LinkProps>`
+  ${color};
+  ${fontFamily};
+  ${fontSize};
+  ${fontWeight};
+  ${lineHeight};
+`;
+
+export const FooterLink = styled.a<LinkProps>`
+  ${fontFamily};
+  ${fontSize};
+  line-height: 1.5;
+`;
